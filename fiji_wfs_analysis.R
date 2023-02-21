b012a8a9095940a5975d0a364f4251fe744b0695
Haileygu99 <haileygu497@gmail.com>
2023-02-21 17:52:25 +0000

    VIF added

--- fiji_wfs_analysis.R
+++ fiji_wfs_analysis.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 
 
 # Load the data
@@ -18,23 +19,22 @@ fiji3<-subset(fiji2, exposure_status != "Self-spouse sterlzed")
 fiji4<-subset(fiji3, exposure_status != "Not exposed")
 
 
-# Create the a column where all the "Not using" and "Using inefficient" will return 0, and others return 1. 
+# Create the a column where all the "Not using" and "Using inefficient" will return 1, and others return 0. 
 wfs_contra_groups <- fiji4 %>%
   mutate(
-    contra_binary = ifelse(curr_contracept %in% c("Not using","Using inefficient"),
+    contra_binary = ifelse(curr_contracept %in% c("Using efficient"),
                            0,1)
   )
 
+####-----------------------------------------------------------------------------
 
-# Removing all the NA's in the column "region"
-# All the 0s in the contra_binary --> "Not using", 1s --> "Using"
 wfs_contra_groups <- wfs_contra_groups %>% 
   filter(!is.na(region)) %>% # remove rows with NA in region column
-  mutate(contra_binary = ifelse(contra_binary == 0, "Not using", "Using"))
-
+####-----------------------------------------------------------------------------
 
 ###### Descriptives 
 #summary of the characteristics
+
 summary(wfs_contra_groups)
 sd(wfs_contra_groups$age)
 
@@ -63,6 +63,8 @@ ggplot(wfs_contra_groups, aes(x = factor(contra_binary), fill = factor(contra_bi
 # create a contingency table of the two variables "region" and "contra_binary"
 # the resulting table shows the count of observations in each combination of categories between the two variables.
 wfs_contra_table<-table(wfs_contra_groups$region, wfs_contra_groups$contra_binary)
+
+
 wfs_contra_table
 
 
@@ -91,16 +93,129 @@ chisq.test(wfs_contra_table)
 ### Are differences in contraceptive uptake in different regions explained by differences in ethnicity in fecund women?
 ## H0: The odds ratios of not using contraceptives between different regions is equal to 1.
 ## H1: The odds ratio of not using contraceptives between regions is not equal to 1. 
+# Create the region variable as a factor with "Western" as the reference category
+wfs_contra_groups$region <- factor(wfs_contra_groups$region, levels = c("Western", "Eastern", "Northern", "Central"))
+
+
+model <- glm(contra_binary ~ region, wfs_contra_groups, family = "binomial")
+
+# print the model summary 
+summary(model)
+
+# summary 
+p_values <- summary(model)$coefficients [,"Pr(>|z|)"]
+print(p_values)
+
+# Extract the coefficients and standard errors from the model fit summary 
+coefs <- coef(model)
+se <- sqrt(diag(vcov(model)))
+
+print(coefs)
+print(se)
+
+# calculate the ORs and CIs 
+ors <- exp(coefs)
+ci <- exp(coefs -(1.96 * se))
+
+print(ors)
+print(ci)
+
+or_df <- data.frame(predictor = names(coefs),
+                    or = ors,
+                    lower = ci,
+                    upper = exp(coefs + (1.96 * se)))
+
+### I don't want the intercept to be shown in the graph so I am going to delete the first row in the dataframe.
+or_df <- or_df[-1,]
+
+# create a forest plot 
+ggplot(or_df, aes(y = predictor, x = or, xmin = lower, xmax = upper)) +
+  geom_pointrange() +
+  labs(x = "OR (95% CI)", y = "") + 
+  geom_vline(xintercept = 1, linetype = "dashed", alpha = 0.5) +
+  theme_classic() +
+  annotate("text", x = .8, y = 6.4, label = "") + 
+  annotate("text", x = 1.2, y = 6.4, label = "") +
+  annotate("text", x = 2, y = 6.8, label = "")
+
+
+
+####-----------------------------------------------------------------------
+### logistic regression of contraceptive uptake adjusted for ethnicity 
+model1 <- glm(contra_binary ~ region + ethnicity, wfs_contra_groups, family = "binomial")
+
+
+
+# print the model summary 
+summary(model1)
+
+# summary 
+p_values1 <- summary(model1)$coefficients [,"Pr(>|z|)"]
+print(p_values1)
+
+# Extract the coefficients and standard errors from the model fit summary 
+coefs1 <- coef(model1)
+se1 <- sqrt(diag(vcov(model1)))
+
+print(coefs1)
+print(se1)
+
+# calculate the ORs and CIs 
+ors1 <- exp(coefs1)
+ci1 <- exp(coefs1 -(1.96 * se1))
+
+print(ors1)
+print(ci1)
+
+or_df1 <- data.frame(predictor = names(coefs1),
+                    or = ors1,
+                    lower = ci1,
+                    upper = exp(coefs1 + (1.96 * se1)))
+
+
+
+or_df1 <- or_df1[-9,]
+or_df1 <- or_df1[-9,]
+
+# create a forest plot 
+ggplot(or_df1, aes(y = predictor, x = or, xmin = lower, xmax = upper)) +
+  geom_pointrange() +
+  labs(x = "OR (95% CI)", y = "") + 
+  geom_vline(xintercept = 1, linetype = "dashed", alpha = 0.5) +
+  theme_classic() +
+  annotate("text", x = .8, y = 6.4, label = "") + 
+  annotate("text", x = 1.2, y = 6.4, label = "") +
+  annotate("text", x = 2, y = 6.8, label = "") +
+  scale_x_continuous(limits = c(0, 3), breaks = seq(0, 3, by = 0.5))
+
+
+
+        
+##### Use VIF to test collinearity
+library(car)
+model2 <- glm(contra_binary ~ region + religion + ethnicity + age + education + education + occupation, data = wfs_contra_groups, family = "binomial")
+vif(model2)
+
+# Calculate the VIF values
+vif_values <- vif(model2)
+
+# Assuming the original data frame has columns 'a', 'b', 'c', 'd', 'e'
+vif_values <- c(3, 7, 7, 1, 3, 9)
+names(vif_values) <- c('region', 'religion', 'ethnicity', 'age', 'education', 'occupation')
+vif_df <- data.frame(predictor = names(vif_values), vif = vif_values)
+
+# Plot the VIFs as a bar plot
+ggplot(vif_df, aes(x = predictor, y = vif)) +
+  geom_bar(stat = 'identity', fill = 'steelblue', width = 0.5) +
+  coord_flip() + 
+  labs(x = '', y = 'VIF') + 
+  theme_minimal() + 
+  geom_hline(yintercept = 5, linetype = 'dashed', color = 'red', alpha = 0.7)
 
 
 
 
-### ---------------------------------------------------------
-wfs_religion_table<-table(wfs_contra_groups$religion, wfs_contra_groups$contra_binary)
-wfs_religion_table
 
-
-chisq.test(wfs_religion_table)
 
 
    
